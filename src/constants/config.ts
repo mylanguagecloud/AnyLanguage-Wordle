58a41e6b588de0b0cb7d7320faea6a5988ade68f
mylanguagecloud <pharrington@internode.on.net>
2024-02-20 18:50:56 +1200

    version 1.0

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,10 +1,10 @@
 export const CONFIG = {
   tries: 6, // This changes how many tries you get to finish the wordle
-  language: 'YourLanguageHere', // This changes the display name for your language
-  wordLength: 5, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
-  author: 'YourNameHere', // Put your name here so people know who made this Wordle!
-  authorWebsite: 'YourLinkHere', // Put a link to your website or social media here
-  wordListSource: 'YourDictionarySource', // Describe the source material for your words here
+  language: Norf'k, // This changes the display name for your language
+  wordLength: 6, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
+  author: Paul Harrington, // Put your name here so people know who made this Wordle!
+  authorWebsite: https://language.norfk.info/, // Put a link to your website or social media here
+  wordListSource: Council of Elders Language Commission, // Describe the source material for your words here
   wordListSourceLink: 'YourDictionaryLink', // Put a link to the source material for your words here
   //
   // THESE NEXT SETTINGS ARE FOR ADVANCED USERS
--- src/constants/orthography.ts
+++ src/constants/orthography.ts
@@ -2,54 +2,41 @@ import { CONFIG } from './config'
 
 export const ORTHOGRAPHY = [
   'a',
-  'aa',
   'b',
   'd',
   'e',
-  'ee',
+'f',
   'g',
-  'g̱',
-  'gw',
-  'gy',
+  
   'h',
-  'hl',
   'i',
-  'ii',
+ 
   'j',
   'k',
-  'ḵ',
-  "k'",
-  "ḵ'",
-  'kw',
-  "kw'",
-  'ky',
-  "ky'",
+ 
   'l',
-  "'l",
+  
   'm',
-  "'m",
+  
   'n',
-  "'n",
+  
   'o',
-  'oo',
+  
   'p',
-  "p'",
+  'r',
   's',
   't',
-  "t'",
-  'ts',
-  "ts'",
+  
   'u',
-  'uu',
+  
   'w',
-  "'w",
+  
   'x',
-  'x̱',
-  'xw',
+  
   'y',
-  "'y",
+  'z',
   "'",
-  '?',
+  
 ]
 
 if (CONFIG.normalization) {
