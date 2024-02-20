4c50367df3e50e96d8f7c8a35bc68c8f973a0146
mylanguagecloud <pharrington@internode.on.net>
2024-02-20 19:03:32 +1200

    Version 1.4

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -3,7 +3,7 @@ export const CONFIG = {
   language: Norfolk, // This changes the display name for your language
   wordLength: 5, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
   author: Paul Harrington, // Put your name here so people know who made this Wordle!
-  authorWebsite: https://language.norfk.info/, // Put a link to your website or social media here
+  authorWebsite: language.norfk.info/, // Put a link to your website or social media here
   wordListSource: Council of Elders Language Commission, // Describe the source material for your words here
   wordListSourceLink: 'YourDictionaryLink', // Put a link to the source material for your words here
   //
