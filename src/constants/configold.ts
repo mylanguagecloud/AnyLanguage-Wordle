0f7c418647f94f4b6b57310452a08de7b366f749
mylanguagecloud <pharrington@internode.on.net>
2024-02-20 19:11:38 +1200

    version 1.5

--- src/constants/configold.ts
+++ src/constants/configold.ts
@@ -1,10 +1,10 @@
 export const CONFIG = {
   tries: 6, // This changes how many tries you get to finish the wordle
-  language: 'YourLanguageHere', // This changes the display name for your language
+  language: Norfolk, // This changes the display name for your language
   wordLength: 5, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
-  author: 'YourNameHere', // Put your name here so people know who made this Wordle!
-  authorWebsite: 'YourLinkHere', // Put a link to your website or social media here
-  wordListSource: 'YourDictionarySource', // Describe the source material for your words here
+  author: Paul Harrington, // Put your name here so people know who made this Wordle!
+  authorWebsite: language.norfk.info/, // Put a link to your website or social media here
+  wordListSource: Council of Elders Language Commission, // Describe the source material for your words here
   wordListSourceLink: 'YourDictionaryLink', // Put a link to the source material for your words here
   //
   // THESE NEXT SETTINGS ARE FOR ADVANCED USERS
@@ -12,7 +12,7 @@ export const CONFIG = {
   googleAnalytics: '', // You can use this if you use Google Analytics
   shuffle: false, // whether to shuffle the words in the wordlist each time you load the app (note: you will lose the 'word of the day' functionality if this is true)
   normalization: 'NFC', // whether to apply Unicode normalization to words and orthography - options: 'NFC', 'NFD', 'NKFC', 'NKFD', false
-  startDate: 'January 1, 2022 00:00:00', // what date and time to start your game from
+  startDate: 'February 29, 2024 00:00:00', // what date and time to start your game from
   defaultLang: 'en', // the default interface language
   availableLangs: ['en', 'es', 'sw', 'zh'], // the options available to the user for translation languages
   escapeSpecialCharacters: true, // whether to escape all characters in the orthography.
